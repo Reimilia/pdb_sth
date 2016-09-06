e4d11670ed8ebd8c4de0ce956aa82b122f89990f
wy <panzer.wy@gmail.com>
2016-09-06 14:30:04 -0400

    rdkit usage, merge 2 function into 1

--- sdf_reader.py
+++ sdf_reader.py
@@ -2,7 +2,7 @@ __author__ = 'wy'
 
 from rdkit import Chem
 import json
-import os,io
+import os, io
 
 '''
 Basic example of how to use rdkit library
@@ -10,127 +10,110 @@ need to first install it follow the instruction on its website
 
 '''
 
-'''
-suppl = Chem.SDMolSupplier('BindingDBSampleSmall.sdf')
-
-for mol in suppl:
-    dict={}
-    for name in mol.GetPropNames():
-        dict[name]= mol.GetProp(name)
-    print json.dumps(dict,indent=4)
-    print Chem.MolToSmiles(mol)
-    print '\n\n\n'
-'''
-
-def query_by_group(suppl, key, value=None):
+def query_specific_field(src , key, value=None):
     '''
-    This program will search list suppl get all required part into one single bundle.
-    :param suppl: a Chem.SDMolSupplier or a list of Mol
+    This program will search sdf file in the specifc location or search in group
+    and pick up the specific field u want from the extension part in a single entry
+    of a sdf file or a group
+
+    From now on it only supports basic one-field search
     :param key: the search field, need exact match
     :param value: the target value, can be a part in the string (e.g. one pdb id in a string)
-    :return: the bundle and write into a single file
+    :param src: two possible type :
+                first : a filelocation must end in .sdf otherwise the result is nothing but '[]'
+                second: a group of molecules (entries) , can be a list or bundle resource from rdkit library
+                (i.e. the Chem.SDMolSupplier
+    :return: the bundle result as a list, each is a mol in redit class-object format
+             also, the result will be written in to a file in 'result' directory as a sdf file
     '''
-    ans_list=[]
+
+    # write the result
     dir = 'result/filter_{}_{}'.format(key.split(' ')[0] + key.split(' ')[-1], value) + '.sdf'
     if not os.path.isfile(dir):
         if not os.path.exists('result'):
             os.mkdir('result')
+
+    # sdf writer
     w = Chem.SDWriter(dir)
+
+    # first possible input : filename and its location
+    if isinstance(src,str):
+        try:
+            sdf = Chem.SDMolSupplier(src)
+        except:
+            print 'filename is wrong!'
+            raise IOError
+    else:
+        sdf= src
+
+    assert isinstance(key, str)
+    # This list will return all molecules that satisfy the needs
+    ans_list = []
     try:
-        for mol in suppl:
-            assert isinstance(key, str)
-            assert mol is not None
+        for mol in sdf:
+            if mol is None:
+                print 'Incomplete data?'
+                continue
             if mol.HasProp(key):
                 src_value = mol.GetProp(key)
 
-            # write and store the corresponding files
-            if value is None:
-                ans_list.append(mol)
-                w.write(mol)
-            else:
-
-                if value == src_value or (isinstance(value, str) and isinstance(src_value, str) and value in src_value):
-                    ans_list.append(mol)
-                    w.write(mol)
-    except:
-        raise TypeError
-
-    return ans_list
-
-def query_by_file(filedir, filename, key, value=None):
-    '''
-    This program will search sdf file in the specifc location and
-    get all required part into one single bundle.
-
-    From now on it only supports basic one field search
-    :param key: the search field, need exact match
-    :param value: the target value, can be a part in the string (e.g. one pdb id in a string)
-    :param filename: the name of file , '*' means search all
-    :param filedir: the location of a specific file
-    :return: the bundle and write into a single file
-    '''
-
-    dir = 'result/filter_{}_{}'.format(key.split(' ')[0]+key.split(' ')[-1],value)+'.sdf'
-    if not os.path.isfile(dir):
-        if not os.path.exists('result'):
-            os.mkdir('result')
-    w = Chem.SDWriter(dir)
-    if filename=='*':
-        listedfile = os.listdir(filedir)
-    else:
-        listedfile = [os.path.join(filedir,filename)]
-    #This list will return all molecules that satisfy the needs
-    ans_list = []
-    for filename in listedfile:
-        if filename.split('.')[-1] == 'sdf':
-            try:
-                sdf = Chem.SDMolSupplier(os.path.join(filedir,filename))
-            except:
-                raise IOError
-            for mol in sdf:
-                assert isinstance(key,str)
-                assert mol is not None
-                if mol.HasProp(key):
-                    src_value= mol.GetProp(key)
-
-                #write and store the corresponding files
+                # write and store the corresponding files
                 if value is None:
                     ans_list.append(mol)
                     w.write(mol)
                 else:
-
-                    if value==src_value or (isinstance(value,str) and isinstance(src_value,str) and value in src_value):
+                    if value == src_value or (
+                                    isinstance(value, str) and isinstance(src_value, str) and value in src_value):
                         ans_list.append(mol)
                         w.write(mol)
+    except:
+        raise TypeError
     return ans_list
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
+
+    #Both are correct usage
 
 
+    sdf = Chem.SDMolSupplier('BindingDBSampleBig.sdf')
+    filtered_result = query_specific_field(sdf, 'PDB ID(s) for Ligand-Target Complex', '2H6T')
 
-    filtered_result= query_by_file('.','BindingDBSampleBig.sdf','PDB ID(s) for Ligand-Target Complex','2H6T')
+    filtered_result = query_specific_field('BindingDBSampleBig.sdf', 'PDB ID(s) for Ligand-Target Complex', '2H6T')
+
+
+    #Test
+    #You need to manually change the address to see what's going on
+    sdf = Chem.SDMolSupplier('/media/wy/data/BindingDB_All_terse_3D.sdf')
+    count =0
+    for mol in sdf:
+        if mol is not None:
+            count+=1
+    print count
+
+
+    # Basic usage if this library
     for mol in filtered_result:
-        #This will show how to get protery names , basic info in a molecule class
-        #For more info, see the website of rdkit
+        # This will show how to get protery names , basic info in a molecule class
+        # For more info, see the website of rdkit
 
-        #dict for property
-        dict={}
+        # dict for property
+        dict = {}
         for name in mol.GetPropNames():
-            dict[name]= mol.GetProp(name)
-        print json.dumps(dict,indent=4)
+            dict[name] = mol.GetProp(name)
+        print json.dumps(dict, indent=4)
 
-        #SMILE format
+        # SMILE format
         print Chem.MolToSmiles(mol)
 
-        #numofAtoms
+        # numofAtoms
         print mol.GetNumAtoms()
 
-        #Atom and bonds
+        # Atom and bonds
         print mol.GetAtomWithIdx(0).GetSymbol()
         print mol.GetAtomWithIdx(0).GetExplicitValence()
         print mol.GetBondWithIdx(0).GetBeginAtomIdx()
         print mol.GetBondWithIdx(0).GetEndAtomIdx()
         print mol.GetBondBetweenAtoms(0, 1).GetBondType()
 
-        #This is useless for getting info
-        print mol
+        # This is useless for getting info
