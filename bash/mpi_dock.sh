a3dfdfa9367ccbddf9eb504e468555d293c17c00
wy <panzer.wy@gmail.com>
2016-10-22 00:32:55 -0400

    organize bash files update mpi part for docking result

---
+++ bash/mpi_dock.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#BSUB -a openmpi
+#BSUB -n 100
+#BSUB -W 100:00
+#BSUB -J mpi_fast
+#BSUB -o /home/yw174/job/mpi_fast.out
+#BSUB -e /home/yw174/job/mpi_fast.err
+#BSUB -q mpi
+module load dev/openmpi-1.8.6
+export OMP_NUM_THREADS=1
+export PATH=$PATH:/home/yw174/usr/babel/bin/
+source /home/yw174/python_env/wy/bin/activate
+cd /home/yw174/program/pdb_sth
+mpirun -np 2 python dockingbroadcast.py
